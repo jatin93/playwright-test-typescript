6137b2f3a7eb5738688b7e0f2ea964af6e4e5322
Jateen Sharma <jateen.sharma@thoughtworks.com>
2022-09-14 13:11:53 +0530

    [Jatin] Change headless flag.

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 const config: PlaywrightTestConfig = {
   testDir: "./tests",
 
-  use: { baseURL: "http://demo.realworld.io", headless: false, screenshot: "only-on-failure" },
+  use: { baseURL: "http://demo.realworld.io", headless: true, screenshot: "only-on-failure" },
   timeout: 30000,
   expect: { timeout: 5000 },
   reporter: [["html", { outputFolder: "test-report" }]],
